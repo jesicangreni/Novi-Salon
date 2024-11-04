dcb121730badb8ed84196e9276f0fb4e99756ad8
bryanvans <bryanaja92@gmail.com>
2024-11-05 01:33:19 +0700

    menyelesaikan tampilan homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -16,6 +16,7 @@
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -89,10 +90,10 @@
                   <img src="assets\img\salon1.jpeg" alt="Image" class="img-fluid">
                 </div>
                 <div class="swiper-slide">
-                  <img src="public\assets\img\salon2.jpeg" alt="Image" class="img-fluid">
+                  <img src="assets\img\salon2.jpeg" alt="Image" class="img-fluid">
                 </div>
                 <div class="swiper-slide">
-                  <img src="public\assets\img\salon3.jpeg" alt="Image" class="img-fluid">
+                  <img src="assets\img\salon3.jpeg" alt="Image" class="img-fluid">
                 </div>
               </div>
               <div class="swiper-pagination"></div>
@@ -108,110 +109,169 @@
       </div>
     </section><!-- /About Section -->
 
+<!-- Perawatan & Pelayanan Section -->
+<section id="perawatan-pelayanan" class="perawatan-pelayanan section">
+  
+    <h3>PERAWATAN & LAYANAN</h3>
+<!-- Card Container -->
+<div class="card-container">
+    <div class="card" style="width: 18rem;">
+        <img src="assets/img/salon2.jpeg" alt="Image" class="img-fluid card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">Creambath Rambut</h5>
+            <p class="card-text">Rasakan sensasi relaksasi dengan creambath rambut kami! Segera jadwalkan kunjunganmu!</p>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="assets/img/salon4.jpeg" alt="Image" class="img-fluid card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">Perawatan Wajah</h5>
+            <p class="card-text">Cantik Dimulai dari Sini! Dapatkan perawatan wajah terbaik hanya di Novi-Salon.</p>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="assets/img/salon5.jpeg" alt="Image" class="img-fluid card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">Manicure</h5>
+            <p class="card-text">Manicure yang Menawan, Hanya di Novi-Salon! Jangan lewatkan!</p>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="assets/img/salon6.jpeg" alt="Image" class="img-fluid card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">Cat Rambut</h5>
+            <p class="card-text">Tampil beda dengan warna rambut yang menawan hanya di Novi-Salon!</p>
+        </div>
+    </div>
+</div>
+
+</section>
+<!-- /Perawatan & Pelayanan Section -->
+
+
+
+
+
     <!-- Testimonials Section -->
     <section id="testimonials" class="testimonials section">
-      <!-- Section Title -->
-      <div class="container section-title" data-aos="fade-up">
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
         <p>Happy Customers</p>
         <h2>Testimonials</h2>
-      </div><!-- End Section Title -->
+    </div><!-- End Section Title -->
 
-      <div class="container" data-aos="fade-up">
+    <div class="container" data-aos="fade-up">
         <div class="row justify-content-center">
-          <div class="col-lg-7">
-            <div class="swiper init-swiper">
-              <script type="application/json" class="swiper-config">
-                {
-                  "loop": true,
-                  "speed": 600,
-                  "autoplay": {
-                    "delay": 5000
-                  },
-                  "slidesPerView": "auto",
-                  "pagination": {
-                    "el": ".swiper-pagination",
-                    "type": "bullets",
-                    "clickable": true
-                  },
-                  "breakpoints": {
-                    "320": {
-                      "slidesPerView": 1,
-                      "spaceBetween": 40
-                    },
-                    "1200": {
-                      "slidesPerView": 1,
-                      "spaceBetween": 1
-                    }
-                  }
-                }
-              </script>
-              <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                  <div class="testimonial mx-auto">
-                    <h3 class="name">Dewi</h3>
-                    <blockquote>
-                      <p>
-                        “Novi Salon benar-benar tempat favorit saya untuk merawat diri. Dari potongan rambut hingga perawatan kulit, semuanya dikerjakan dengan profesional dan penuh perhatian. Hasilnya selalu memuaskan, membuat saya merasa lebih percaya diri setiap kali selesai perawatan!”
-                      </p>
-                    </blockquote>
-                  </div>
+            <div class="col-lg-7">
+                <div class="swiper init-swiper">
+                    <script type="application/json" class="swiper-config">
+                        {
+                          "loop": true,
+                          "speed": 600,
+                          "autoplay": {
+                            "delay": 5000
+                          },
+                          "slidesPerView": "auto",
+                          "pagination": {
+                            "el": ".swiper-pagination",
+                            "type": "bullets",
+                            "clickable": true
+                          },
+                          "navigation": {
+                            "nextEl": ".swiper-button-next",
+                            "prevEl": ".swiper-button-prev"
+                          },
+                          "breakpoints": {
+                            "320": {
+                              "slidesPerView": 1,
+                              "spaceBetween": 40
+                            },
+                            "1200": {
+                              "slidesPerView": 1,
+                              "spaceBetween": 1
+                            }
+                          }
+                        }
+                    </script>
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="testimonial mx-auto">
+                                <h3 class="name">Dewi</h3>
+                                <blockquote>
+                                    <p>
+                                        “Novi Salon benar-benar tempat favorit saya untuk merawat diri. Dari potongan rambut hingga perawatan kulit, semuanya dikerjakan dengan profesional dan penuh perhatian. Hasilnya selalu memuaskan, membuat saya merasa lebih percaya diri setiap kali selesai perawatan!”
+                                    </p>
+                                </blockquote>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="testimonial mx-auto">
+                                <h3 class="name">Elisa</h3>
+                                <blockquote>
+                                    <p>
+                                        “Taya suka bagaimana Novi Salon selalu mengikuti tren terbaru dalam gaya rambut dan makeup. Mereka sangat memahami kebutuhan saya dan memberikan rekomendasi yang tepat. Pelayanannya ramah, dan suasana salonnya nyaman. Sangat direkomendasikan!”
+                                    </p>
+                                </blockquote>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="testimonial mx-auto">
+                                <h3 class="name">Kayla</h3>
+                                <blockquote>
+                                    <p>
+                                        “Di Novi Salon, saya selalu mendapatkan pelayanan yang luar biasa. Mereka sangat up-to-date dengan tren kecantikan terbaru, dan setiap kali saya datang, hasilnya selalu melebihi ekspektasi. Plus, suasana salon yang nyaman membuat pengalaman saya semakin menyenangkan.”
+                                    </p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-pagination"></div>
                 </div>
-                <div class="swiper-slide">
-                  <div class="testimonial mx-auto">
-                    <h3 class="name">Elisa</h3>
-                    <blockquote>
-                      <p>
-                        “Taya suka bagaimana Novi Salon selalu mengikuti tren terbaru dalam gaya rambut dan makeup. Mereka sangat memahami kebutuhan saya dan memberikan rekomendasi yang tepat. Pelayanannya ramah, dan suasana salonnya nyaman. Sangat direkomendasikan!”
-                      </p>
-                    </blockquote>
-                  </div>
-                </div>
-                <div class="swiper-slide">
-                  <div class="testimonial mx-auto">
-                    <h3 class="name">Kayla</h3>
-                    <blockquote>
-                      <p>
-                        “Di Novi Salon, saya selalu mendapatkan pelayanan yang luar biasa. Mereka sangat up-to-date dengan tren kecantikan terbaru, dan setiap kali saya datang, hasilnya selalu melebihi ekspektasi. Plus, suasana salon yang nyaman membuat pengalaman saya semakin menyenangkan.”
-                      </p>
-                    </blockquote>
-                  </div>
-                </div>
-              </div>
-              <div class="swiper-pagination"></div>
             </div>
-          </div>
         </div>
-      </div>
-    </section><!-- /Testimonials Section -->
+    </div>
+</section>
+<!-- /Testimonials Section -->
 
   </main>
 
   <footer id="footer" class="footer light-background">
     <div class="container">
-      <div class="row g-4">
-        <div class="col-md-6 col-lg-3 mb-3 mb-md-0">
-          <div class="widget">
-            <h3 class="widget-heading">About Us</h3>
-            <p class="mb-4">
-            Novi Salon menawarkan layanan kecantikan profesional untuk membuat Anda tampil cantik dan percaya diri. Dengan tim ahli, kami siap memberikan perawatan terbaik yang sesuai dengan kebutuhan Anda.
-          </div>
+        <div class="row g-4 justify-content-center">
+            <div class="col-md-6 col-lg-3 mb-3 mb-md-0 text-center">
+                <div class="widget">
+                    <h3 class="widget-heading">About Us</h3>
+                    <p class="mb-4">
+                        Novi Salon menawarkan layanan kecantikan profesional untuk membuat Anda tampil cantik dan percaya diri. Dengan tim ahli, kami siap memberikan perawatan terbaik yang sesuai dengan kebutuhan Anda.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-3 mb-md-0 text-center">
+                <div class="widget">
+                    <h3 class="widget-heading">Hubungi kami</h3>
+                    <ul class="list-unstyled social-icons light mb-3">
+                        <li>
+                            <a href="#"><span class="bi bi-facebook"></span></a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="bi bi-twitter-x"></span></a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="bi bi-instagram"></span></a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="bi bi-linkedin"></span></a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="bi bi-youtube"></span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6 col-lg-3 pl-lg-5">
-          <div class="widget">
-            <h3 class="widget-heading">Hubungi kami</h3>
-            <ul class="list-unstyled social-icons light mb-3">
-              <li>
-                <a href="#"><span class="bi bi-facebook"></span></a>
-              </li>
-              <li>
-                <a href="#"><span class="bi bi-twitter-x"></span></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
     </div>
-  </footer>
+</footer>
+
 
   <!-- Scroll Top -->
   <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
