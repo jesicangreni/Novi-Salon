720fca11f0010636c50783733900ea816bfa8f63
jesicangreni <iss22018@students.del.ac.id>
2024-11-05 01:46:08 +0700

    memperbaiki

--- public/js/navbar.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('.header');
-    header.classList.toggle('scrolled', window.scrollY > 50);
-});
-
-document.querySelector('.mobile-nav-toggle').addEventListener('click', function () {
-    document.querySelector('.navmenu').classList.toggle('active');
-});
